b2db582a8fee7c5cfe3a40a94b5edc002943bc5d
lozunoff <andrey.lozunov@gmail.com>
2020-04-27 20:48:49 +0400

    Создан компонент "ErrorBoundry", отвечающий за перехват ошибок в приложении

---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import ErrorIndicator from '../error-indicator';
+
+export default class ErrorBoundry extends Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+
+    return children;
+  }
+}
+
+ErrorBoundry.propTypes = {
+  children: PropTypes.node.isRequired,
+};
